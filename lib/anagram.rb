d8bdff6d6d7d2534c98dda4779d61769733651eb
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-26 15:02:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,12 +7,6 @@ class Anagram
   end
   
   def match_word(words)
-    empty_array = []
-    words.each do |wordss|
-      if wordss.split(' ').sort == self.wordss.split(' ').sort 
-        empty_array << wordss
-      end
-    end
-    match_word
+    
   end
 end
