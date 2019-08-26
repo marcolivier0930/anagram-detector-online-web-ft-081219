e175840f3a4696d720ebc7e0882bdc4bae2537a3
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-26 14:19:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+def initialize(word)
+  @word = word 
+end
+def match(word)
+  word.each do |e|
+    e.split("") == e.split("")
+  end
+end
+end
